dde84504971ec6e3eaa633451f1b7dc78d2387be
Jose Meroño <josesmerono@gmail.com>
2024-06-15 15:38:53 +0200

    Minor changes

--- src/WaterRenderer.hpp
+++ src/WaterRenderer.hpp
@@ -3,16 +3,16 @@
 #include "types.hpp"
 
 struct WaterMaterial {
-  glm::vec4 diffuseColor = {0.f, 0.1f, 0.4f, 1.f};
+  glm::vec4 diffuseColor = {0.f, 0.2f, 0.25f, 1.f};
   glm::vec2 baseD = {1, 0};
-  float baseA = 0.1;
-  float baseW = 1;
+  float baseA = 0.2;
+  float baseW = 0.5;
 
   uint32_t numFreqs = 60;
   float aMult = 0.8;
   float wMult = 1.2;
 
-  float speed = 2;
+  float speed = 1.6;
 
   float baseReflectivity = 0.02;
 
